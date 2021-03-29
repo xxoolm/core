e1436db861540128db66c6411bee91f9a3557fb8
Martin Hjelmare <marhje52@gmail.com>
2021-03-29 15:48:57 +0200

    Split off base flow

--- homeassistant/components/zwave_js/config_flow.py
+++ homeassistant/components/zwave_js/config_flow.py
@@ -1,6 +1,7 @@
 """Config flow for Z-Wave JS integration."""
 from __future__ import annotations
 
+from abc import abstractmethod
 import asyncio
 import logging
 from typing import Any, cast
@@ -14,7 +15,7 @@ from homeassistant import config_entries, exceptions
 from homeassistant.components.hassio import is_hassio
 from homeassistant.const import CONF_URL
 from homeassistant.core import HomeAssistant, callback
-from homeassistant.data_entry_flow import AbortFlow, FlowResult
+from homeassistant.data_entry_flow import AbortFlow, FlowHandler, FlowResult
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .addon import AddonError, AddonManager, get_addon_manager
@@ -79,7 +80,7 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
 
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
-    ) -> dict[str, Any]:
+    ) -> FlowResult:
         """Manage the options."""
         if user_input is not None:
             use_addon = user_input.get(CONF_USE_ADDON)
@@ -121,10 +122,8 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
         )
 
 
-class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Z-Wave JS."""
-
-    VERSION = 1
+class BaseZwaveJSFlow(FlowHandler):
+    """Represent the base config flow for Z-Wave JS."""
 
     def __init__(self) -> None:
         """Set up flow instance."""
@@ -136,88 +135,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self.integration_created_addon = False
         self.install_task: asyncio.Task | None = None
         self.start_task: asyncio.Task | None = None
-
-    @staticmethod
-    @callback
-    def async_get_options_flow(
-        config_entry: config_entries.ConfigEntry,
-    ) -> OptionsFlowHandler:
-        """Return the options flow."""
-        return OptionsFlowHandler(config_entry)
-
-    async def async_step_user(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
-        """Handle the initial step."""
-        if is_hassio(self.hass):
-            return await self.async_step_on_supervisor()
-
-        return await self.async_step_manual()
-
-    async def async_step_manual(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
-        """Handle a manual configuration."""
-        if user_input is None:
-            return self.async_show_form(
-                step_id="manual", data_schema=STEP_USER_DATA_SCHEMA
-            )
-
-        errors = {}
-
-        try:
-            version_info = await validate_input(self.hass, user_input)
-        except InvalidInput as err:
-            errors["base"] = err.error
-        except Exception:  # pylint: disable=broad-except
-            _LOGGER.exception("Unexpected exception")
-            errors["base"] = "unknown"
-        else:
-            await self.async_set_unique_id(
-                version_info.home_id, raise_on_progress=False
-            )
-            # Make sure we disable any add-on handling
-            # if the controller is reconfigured in a manual step.
-            self._abort_if_unique_id_configured(
-                updates={
-                    **user_input,
-                    CONF_USE_ADDON: False,
-                    CONF_INTEGRATION_CREATED_ADDON: False,
-                }
-            )
-            self.ws_address = user_input[CONF_URL]
-            return self._async_create_entry_from_vars()
-
-        return self.async_show_form(
-            step_id="manual", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
-        )
-
-    async def async_step_hassio(self, discovery_info: dict[str, Any]) -> FlowResult:
-        """Receive configuration from add-on discovery info.
-
-        This flow is triggered by the Z-Wave JS add-on.
-        """
-        self.ws_address = f"ws://{discovery_info['host']}:{discovery_info['port']}"
-        try:
-            version_info = await async_get_version_info(self.hass, self.ws_address)
-        except CannotConnect:
-            return self.async_abort(reason="cannot_connect")
-
-        await self.async_set_unique_id(version_info.home_id)
-        self._abort_if_unique_id_configured(updates={CONF_URL: self.ws_address})
-
-        return await self.async_step_hassio_confirm()
-
-    async def async_step_hassio_confirm(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
-        """Confirm the add-on discovery."""
-        if user_input is not None:
-            return await self.async_step_on_supervisor(
-                user_input={CONF_USE_ADDON: True}
-            )
-
-        return self.async_show_form(step_id="hassio_confirm")
 
     @callback
     def _async_create_entry_from_vars(self) -> FlowResult:
@@ -232,30 +149,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 CONF_INTEGRATION_CREATED_ADDON: self.integration_created_addon,
             },
         )
-
-    async def async_step_on_supervisor(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
-        """Handle logic when on Supervisor host."""
-        if user_input is None:
-            return self.async_show_form(
-                step_id="on_supervisor", data_schema=ON_SUPERVISOR_SCHEMA
-            )
-        if not user_input[CONF_USE_ADDON]:
-            return await self.async_step_manual()
-
-        self.use_addon = True
-
-        if await self._async_is_addon_running():
-            addon_config = await self._async_get_addon_config()
-            self.usb_path = addon_config[CONF_ADDON_DEVICE]
-            self.network_key = addon_config.get(CONF_ADDON_NETWORK_KEY, "")
-            return await self.async_step_finish_addon_setup()
-
-        if await self._async_is_addon_installed():
-            return await self.async_step_configure_addon()
-
-        return await self.async_step_install_addon()
 
     async def async_step_install_addon(
         self, user_input: dict[str, Any] | None = None
@@ -374,6 +267,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 self.hass.config_entries.flow.async_configure(flow_id=self.flow_id)
             )
 
+    @abstractmethod
     async def async_step_finish_addon_setup(
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
@@ -382,27 +276,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         Get add-on discovery info and server version info.
         Set unique id and abort if already configured.
         """
-        if not self.ws_address:
-            discovery_info = await self._async_get_addon_discovery_info()
-            self.ws_address = f"ws://{discovery_info['host']}:{discovery_info['port']}"
-
-        if not self.unique_id:
-            try:
-                version_info = await async_get_version_info(self.hass, self.ws_address)
-            except CannotConnect as err:
-                raise AbortFlow("cannot_connect") from err
-            await self.async_set_unique_id(
-                version_info.home_id, raise_on_progress=False
-            )
-
-        self._abort_if_unique_id_configured(
-            updates={
-                CONF_URL: self.ws_address,
-                CONF_USB_PATH: self.usb_path,
-                CONF_NETWORK_KEY: self.network_key,
-            }
-        )
-        return self._async_create_entry_from_vars()
 
     async def _async_get_addon_info(self) -> dict:
         """Return and cache Z-Wave JS add-on info."""
@@ -460,6 +333,148 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             raise AbortFlow("addon_get_discovery_info_failed") from err
 
         return discovery_info_config
+
+
+class ConfigFlow(config_entries.ConfigFlow, BaseZwaveJSFlow, domain=DOMAIN):
+    """Handle a config flow for Z-Wave JS."""
+
+    VERSION = 1
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(
+        config_entry: config_entries.ConfigEntry,
+    ) -> OptionsFlowHandler:
+        """Return the options flow."""
+        return OptionsFlowHandler(config_entry)
+
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle the initial step."""
+        if is_hassio(self.hass):
+            return await self.async_step_on_supervisor()
+
+        return await self.async_step_manual()
+
+    async def async_step_manual(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle a manual configuration."""
+        if user_input is None:
+            return self.async_show_form(
+                step_id="manual", data_schema=STEP_USER_DATA_SCHEMA
+            )
+
+        errors = {}
+
+        try:
+            version_info = await validate_input(self.hass, user_input)
+        except InvalidInput as err:
+            errors["base"] = err.error
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.exception("Unexpected exception")
+            errors["base"] = "unknown"
+        else:
+            await self.async_set_unique_id(
+                version_info.home_id, raise_on_progress=False
+            )
+            # Make sure we disable any add-on handling
+            # if the controller is reconfigured in a manual step.
+            self._abort_if_unique_id_configured(
+                updates={
+                    **user_input,
+                    CONF_USE_ADDON: False,
+                    CONF_INTEGRATION_CREATED_ADDON: False,
+                }
+            )
+            self.ws_address = user_input[CONF_URL]
+            return self._async_create_entry_from_vars()
+
+        return self.async_show_form(
+            step_id="manual", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+        )
+
+    async def async_step_hassio(self, discovery_info: dict[str, Any]) -> FlowResult:
+        """Receive configuration from add-on discovery info.
+
+        This flow is triggered by the Z-Wave JS add-on.
+        """
+        self.ws_address = f"ws://{discovery_info['host']}:{discovery_info['port']}"
+        try:
+            version_info = await async_get_version_info(self.hass, self.ws_address)
+        except CannotConnect:
+            return self.async_abort(reason="cannot_connect")
+
+        await self.async_set_unique_id(version_info.home_id)
+        self._abort_if_unique_id_configured(updates={CONF_URL: self.ws_address})
+
+        return await self.async_step_hassio_confirm()
+
+    async def async_step_hassio_confirm(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Confirm the add-on discovery."""
+        if user_input is not None:
+            return await self.async_step_on_supervisor(
+                user_input={CONF_USE_ADDON: True}
+            )
+
+        return self.async_show_form(step_id="hassio_confirm")
+
+    async def async_step_on_supervisor(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle logic when on Supervisor host."""
+        if user_input is None:
+            return self.async_show_form(
+                step_id="on_supervisor", data_schema=ON_SUPERVISOR_SCHEMA
+            )
+        if not user_input[CONF_USE_ADDON]:
+            return await self.async_step_manual()
+
+        self.use_addon = True
+
+        if await self._async_is_addon_running():
+            addon_config = await self._async_get_addon_config()
+            self.usb_path = addon_config[CONF_ADDON_DEVICE]
+            self.network_key = addon_config.get(CONF_ADDON_NETWORK_KEY, "")
+            return await self.async_step_finish_addon_setup()
+
+        if await self._async_is_addon_installed():
+            return await self.async_step_configure_addon()
+
+        return await self.async_step_install_addon()
+
+    async def async_step_finish_addon_setup(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Prepare info needed to complete the config entry.
+
+        Get add-on discovery info and server version info.
+        Set unique id and abort if already configured.
+        """
+        if not self.ws_address:
+            discovery_info = await self._async_get_addon_discovery_info()
+            self.ws_address = f"ws://{discovery_info['host']}:{discovery_info['port']}"
+
+        if not self.unique_id:
+            try:
+                version_info = await async_get_version_info(self.hass, self.ws_address)
+            except CannotConnect as err:
+                raise AbortFlow("cannot_connect") from err
+            await self.async_set_unique_id(
+                version_info.home_id, raise_on_progress=False
+            )
+
+        self._abort_if_unique_id_configured(
+            updates={
+                CONF_URL: self.ws_address,
+                CONF_USB_PATH: self.usb_path,
+                CONF_NETWORK_KEY: self.network_key,
+            }
+        )
+        return self._async_create_entry_from_vars()
 
 
 class CannotConnect(exceptions.HomeAssistantError):
